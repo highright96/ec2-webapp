c95519713234861126db546a87b041cd9a2bdfe9
highright96 <nsw5296@naver.com>
2021-06-24 11:40:40 +0900

    [init] 프로젝트 세팅

---
+++ src/main/java/dev/highright96/ec2webapp/Ec2WebappApplication.java
@@ -0,0 +1,13 @@
+package dev.highright96.ec2webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2WebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/highright96/ec2webapp/Ec2WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.highright96.ec2webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
