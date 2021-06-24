58b9d68e6928d32c4f7ea490a7e45571952d2dc8
highright96 <nsw5296@naver.com>
2021-06-24 12:59:12 +0900

    [Add] HelloController 추가

---
+++ src/main/java/dev/highright96/ec2webapp/controller/HelloController.java
@@ -0,0 +1,16 @@
+package dev.highright96.ec2webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "index";
+    }
+
+}
